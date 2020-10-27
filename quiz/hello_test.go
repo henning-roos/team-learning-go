159ef4f63ea00f17795e8f2ca67ecd060a1eb303
Rebecca Akkila <rebecca.akkila@ericsson.com>
2020-10-27 11:35:48 +0100

    rebecca 2

--- quiz/hello_test.go
+++ quiz/hello_test.go
@@ -14,6 +14,8 @@ func TestHelloFail(t *testing.T) {
 	assert.False(t, !returnTrue(), "Test hello world negative test failed!!!")
 }
 
-func TestStruct(t *testing.T) {​​
-	t.assert.Equal(t, returnStruct(), "", "Didnt return expected")
-}​​
+func TestStruct(t *testing.T) {
+    actualStruct := returnStruct()
+    assert.Equal(t, actualStruct, question{question: "What is 1+1?", rightAnswer: "2", wrongAnswers: [2]string{"1", "54"}}, "Didnt return expected")
+    assert.Equal(t, actualStruct.question, "What is 1+1?", "Not correct question")
+}
