94a2fb970e448aea798b06f2dcdfb31c99093f4c
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2021-02-11 09:11:00 +0100

    Chris was here

--- quiz/game_test.go
+++ quiz/game_test.go
@@ -7,40 +7,32 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-//var testQuestion = Question{
-//	Question:     "Which language is this written in?",
-//	RightAnswer:  "Go",
-//	WrongAnswers: []string{"Python", "Java"},
-//}
-
 type QuizMock struct {
 	mock.Mock
 }
 
 func (quizMock *QuizMock) ReadQuestionsFromJSON(jsonFile string) []Question {
-	//TODO: understand why all calls gets the same parameter
 	args := quizMock.Called(jsonFile)
 	return args.Get(0).([]Question)
 }
 
 func (quizMock *QuizMock) GetAnswerMap(question Question) map[string]string {
-	//TODO: Fix all the return values
-	quizMock.Called()
-	return nil
+	args := quizMock.Called(question)
+	return args.Get(0).(map[string]string)
 }
 
 func (quizMock *QuizMock) GetUserInput(stdin io.Reader) (string, error) {
-	quizMock.Called()
-	return "", nil
+	args := quizMock.Called()
+	return args.String(0), args.Error(1)
 }
 
 func (quizMock *QuizMock) FormatQuestion(question Question, answerMap map[string]string) string {
-	quizMock.Called()
+	args := quizMock.Called()
 	return ""
 }
 
 func (quizMock *QuizMock) Verify(question Question, answerMap map[string]string, userInput string) (bool, error) {
-	quizMock.Called()
+	args := quizMock.Called()
 	return false, nil
 }
 
@@ -57,7 +49,7 @@ func TestGame(t *testing.T) {
 
 	main(quizMock)
 
-	quizMock.AssertCalled(t, "ReadQuestionsFromJSON")
+	quizMock.AssertCalled(t, "ReadQuestionsFromJSON", "questions.json")
 	quizMock.AssertCalled(t, "GetAnswerMap")
 	quizMock.AssertCalled(t, "GetUserInput")
 	quizMock.AssertCalled(t, "FormatQuestion")
